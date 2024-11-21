b6f1f2d20a72f9ff581e8170e18b1dea063eecce
NicoBoni55 <bonifrank55@gmail.com>
2024-11-21 17:58:23 -0300

    Hello World en React

---
+++ Exercise/01-Hello-World/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+
+const name = "Nicolas"
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <h1>Hello World! {name}</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
