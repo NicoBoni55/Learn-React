360547c0cb3e032307acc2731b67b4ed5f8e5e0c
NicoBoni55 <bonifrank55@gmail.com>
2024-11-26 13:23:44 -0300

    Tic Tac Toe en React Segundo Avance

--- Exercise/04-tic-tac-toe/src/App.jsx
+++ Exercise/04-tic-tac-toe/src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
 import './App.css'
 
+// DEFINIR LOS TURNOS
 const TURNS= {
-  X: "×",
-  O: "o"
+  X: "X",
+  O: "O"
 }
 
+// DEFINIR TODAS LAS COMBINACIONES GANADORAS
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
+// DIBUJAR LOS CUADROS
 const Square = ({children, isSelected, updateBoard, index}) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index);
+  }
   return(
-    <div className={className}>
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
@@ -19,9 +36,53 @@ const Square = ({children, isSelected, updateBoard, index}) => {
 
 function App() {
 
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(Array(9).fill(null)) // ESTADO DEL TABLERO
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(TURNS.X) // ESTADO DE LOS TURNOS
+
+  const [winner, setWinner] = useState(null) // ESTADO PARA DEFINIR GANADOR
+
+  // REVISANDO SI HAY GANADOR CON LAS COMBINACIONES
+  const checkWinner = (boardCheck) => {
+    for(const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if (
+        boardCheck[a] && boardCheck[a] === boardCheck[b] && boardCheck[a] === boardCheck[c]
+      ) {
+        return boardCheck[a]
+      }
+    }
+    return null
+  }
+
+  //EMPEZAR DE NUEVO
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+  }
+
+  // ACTUALIZAR EL TABLERO
+  const updateBoard = (index) => {
+    // SI EN EL TABLERO HAY ALGO HACER UN RETURN
+    if (board[index] || winner) return
+
+    // COPIAR Y ACTUALIZAR EL TABLERO
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    // MANEJAR LOS TURNOS DE X u O
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    //VER SI HAY GANADOR
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+  }
 
   return (
     <>
@@ -33,8 +94,10 @@ function App() {
             return(
               <Square 
               key={index} 
-              index={index}>
-                
+              index={index}
+              updateBoard={updateBoard}>
+              
+                {board[index]}
               </Square>
               )
             }
@@ -49,6 +112,28 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+      <button onClick={resetGame}>Restart</button>
+      {
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner === false
+                  ? 'Empate'
+                  : 'Gano:'
+                }
+              </h2>
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
     </>
   )
